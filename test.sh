d995b801026bfffa25fdc4e937c72fa1fa50727a
Timm Friebe <thekid@thekid.de>
2016-01-31 15:49:43 +0100

    Print header and OS

--- test.sh
+++ test.sh
@@ -27,6 +27,11 @@ verify() {
   fi
 }
 
+printf "\033[32;1mXP Runners tests\033[0m\n"
+echo '(c) 2016 https://github.com/xp-runners/spec/'
+uname -a
+echo
+
 case $(uname) in
   CYGWIN*) EXE=xp.exe ;;
   *) EXE=xp ;;
