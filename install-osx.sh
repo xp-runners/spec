7339128932b736f40af0ba3f45cd44d178ff1e09
Timm Friebe <thekid@thekid.de>
2016-01-31 04:01:01 +0100

    Use PHP bundled w/ MacOSX

--- install-osx.sh
+++ install-osx.sh
@@ -10,8 +10,8 @@ echo "Installing Mono ${MONO_VERSION}"
 curl "http://download.mono-project.com/archive/${MONO_VERSION}/macos-10-x86/MonoFramework-MDK-${MONO_VERSION}.macos10.xamarin.x86.pkg" -o mono.pkg
 sudo installer -pkg mono.pkg -target /
 
-echo "Installing ${PHP_VERSION} via Homebrew"
-brew tap homebrew/dupes
-brew tap homebrew/versions
-brew tap homebrew/homebrew-php
-brew install ${PHP_VERSION}
+# echo "Installing ${PHP_VERSION} via Homebrew"
+# brew tap homebrew/dupes
+# brew tap homebrew/versions
+# brew tap homebrew/homebrew-php
+# brew install ${PHP_VERSION}
