a5fc5aae45dedd362b1d1cbfb68e6c493816f339
Timm Friebe <thekid@thekid.de>
2016-02-13 17:31:30 +0100

    Change dependencies now that we no longer have mkbundle'd exes

--- install-linux.sh
+++ install-linux.sh
@@ -5,7 +5,7 @@ set -u
 
 echo "Installing Mono"
 sudo apt-get update
-sudo apt-get install -y libmono-corlib4.5-cil libmono-2.0-1
+sudo apt-get install -y libmono-corlib4.5-cil libmono-system-core4.0-cil
 
 echo "Installing PHP"
 sudo apt-get install -y php5-cli
